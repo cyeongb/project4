e97aec3954c2ad5bc4f9b0eec62406a53dc73554
choeyeongbi <cyeongb@gmail.com>
2025-04-15 00:47:51 +0900

    1부 끝끝

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+let playerState = 'idle'; // 첫 화면 플레이어 상태 
+const dropdown = document.getElementById('animations');
+dropdown.addEventListener('change',function(e){
+    playerState = e.target.value;
+});
+
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d'); // 캔버스 가져옴
 const CANVAS_WIDTH= canvas.width = 600;
@@ -11,17 +17,74 @@ playerImage.src = './assets/shadow_dog.png';
 const spriteWidth = 575;
 const spriteHeight= 523;
 // 스프라이트 이미지 위치
-let frameX = 0;
-let frameY = 0;
+
 let gameFrame = 0;
+
+
 const staggerFrames = 5; // 움직임 frame 수
+const spriteAnimations=[];
+const animationStates=[
+    {
+        name:'idle',
+        frames:7,
+    },
+    {
+        name:'jump',
+        frames:7,
+    },
+    {
+        name:'fall',
+        frames:7,
+    },
+    {
+        name:'run',
+        frames:9,
+    },
+    {
+        name:'dizzy',
+        frames:11,
+    },
+    {
+        name:'sit',
+        frames:5,
+    },
+    {
+        name:'roll',
+        frames:7,
+    },
+    {
+        name:'bite',
+        frames:7,
+    },
+    {
+        name:'ko',
+        frames:12,
+    },
+    {
+        name:'gethit',
+        frames:4,
+    }
+];
+
+animationStates.forEach((state,i)=>{
+    let frames = {
+        loc:[],
+    }
+    for(let j=0;j<state.frames;j++){
+        let positionX = j * spriteWidth;
+        let positionY = i * spriteHeight;
+        frames.loc.push({x:positionX, y:positionY});
+    }
+    spriteAnimations[state.name] = frames;
+});
+console.log("animationStates=>",animationStates);
 
 
 function animate(){
     ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
-    let position = Math.floor(gameFrame / staggerFrames)%6; //0..1..2..3..4..0..
-    frameX = spriteWidth * position;
-    // frameY = spriteHeight * position;
+    let position = Math.floor(gameFrame / staggerFrames) % spriteAnimations[playerState].loc.length; //0..1..2..3..4..0..
+    let frameX = spriteWidth * position;
+    let frameY = spriteAnimations[playerState].loc[position].y;
     ctx.drawImage(playerImage,frameX,frameY,spriteWidth,spriteHeight,
         0,0,spriteWidth,spriteHeight);
 
@@ -35,8 +98,31 @@ function animate(){
 
 animate();
 
+//스프라이트 이미지를 나타냄 for loop으로 구현.
+// spriteAnimations = [
+//     "idle"={
+//         loc:[
+//             {x:0,y:0},
+//             {x:575,y:0},
+//             {x:1150,y:0},
+//             {x:1725,y:0},
+//             {x:2300,y:0},
+//             {x:2875,y:0},
+//             {x:3450,y:0}
+//         ]
+//     },
+//     "jump"={
+//         width:120,
+//         height:120,
+//         loc:[]
+//     },
+//     "run"={
+//         width:1200,
+//         height:1250,
+//         loc:[]
 
-
+//     }
+// ];
 
 
 
